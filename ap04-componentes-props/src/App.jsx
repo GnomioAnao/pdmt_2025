cabc102bfa467b6bc6522b16e7c648f2df631afd
Caio Lima <caiolimacruz@gmail.com>
2025-08-22 17:53:45 -0300

    agora finalizamos com o uso de coleção e maps

--- ap04-componentes-props/src/App.jsx
+++ ap04-componentes-props/src/App.jsx
@@ -4,6 +4,49 @@ import Cartao from "./Cartao"
 import Feedback from "./Feedback"
 
 const App = () => {
+  const pedido = [
+    {
+      data: '21/08/2025',
+      titulo: 'SSD',
+      icone: 'fa-hdd',
+      descricao: 'SSD Kingston' 
+    },
+    {
+      data: '22/08/2025',
+      titulo: 'Concrete Maths',
+      icone: 'fa-book',
+      descricao: 'Livro do Donald Kruth' 
+    },
+    {
+      data: '21/08/2025',
+      titulo: 'Headphone',
+      icone: 'fa-headphones',
+      descricao: 'Headphones Bluetooth' 
+    },
+    {
+      data: '30/07/2025',
+      titulo: 'Câmera',
+      icone: 'fa-camera',
+      descricao: 'Câmera GoPro' 
+    }
+  ]
+
+  const hipopotamos = [
+    {
+      icone:'fa-solid fa-hippo fa-3x'
+    },
+    {
+      icone:'fa-solid fa-hippo fa-2x'
+    },
+    {
+      icone:'fa-solid fa-hippo fa-med'
+    },
+    {
+      icone:'fa-solid fa-hippo fa-med'
+    }
+
+  ]
+
   const componenteFeedback = (
     <Feedback
       textoOK="já chegou"
@@ -13,57 +56,38 @@ const App = () => {
     />
   )
   return <div>
+    {/* criar uma lista de hipopotamos e produzir cada jsx com a função map*/}
     <div>
-      <i class="fa-solid fa-hippo fa-3x"></i>
+      {
+        hipopotamos.map((hipopotamo) =>(
+          <i class={hipopotamo.icone}></i>
+
+        ))
+      }
+      {/* <i class="fa-solid fa-hippo fa-3x"></i>
       <i class="fa-solid fa-hippo fa-2x"></i>
       <i class="fa-solid fa-hippo fa-med"></i>
-      <i class="fa-solid fa-hippo fa-med"></i>
+      <i class="fa-solid fa-hippo fa-med"></i> */}
     </div>
     <div className="container border rounded my-2 p-3">
       <div className="row">
-        <div className="col-sm-12 col-lg-6 col-xxl-4">
-          <Cartao cabecalho="22/05/2025">
-            <Pedido
-              icone="fa-hdd"
-              titulo="SSD"
-              descricao="SSD Kingston" />
-              <div className="mt-3"></div>
-              {componenteFeedback}
-          </Cartao>
-        </div>
+        {
+          pedido.map((pedido) => (
 
         <div className="col-sm-12 col-lg-6 col-xxl-4">
-          <Cartao cabecalho="23/02/2025">
+          <Cartao cabecalho={pedido.data}>
             <Pedido
-              icone="fa-book"
-              titulo="Pequeno principe"
-              descricao="Livro clássico" />
+              icone={pedido.icone}
+              titulo={pedido.titulo}
+              descricao={pedido.descricao} />
               <div className="mt-3"></div>
               {componenteFeedback}
           </Cartao>
+          <div className="mb-3"></div>
         </div>
 
-        <div className="col-sm-12 col-lg-6 col-xxl-4">
-          <Cartao cabecalho="28/02/2025">
-            <Pedido
-              icone="fa-headphones"
-              titulo="Headphone"
-              descricao="Headphone Bluetooth" />
-              <div className="mt-3"></div>
-              {componenteFeedback}
-          </Cartao>
-        </div>
-
-        <div className="col-sm-12 col-lg-6 col-xxl-4">
-          <Cartao cabecalho="03/05/2025">
-            <Pedido
-              icone="fa-camera"
-              titulo="Câmera"
-              descricao="Câmera GoPro" />
-              <div className="mt-3"></div>
-              {componenteFeedback}
-          </Cartao>
-        </div>
+          ))
+        }
 
       </div>
     </div>

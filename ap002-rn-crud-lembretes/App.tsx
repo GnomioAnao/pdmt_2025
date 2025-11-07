5898283587e192866ded9242c874d4911510f9f0
Caio Lima <caiolimacruz@gmail.com>
2025-11-07 18:20:38 -0300

    utilizamos de icones e novas funções: FlatList (ao invés do map)

--- ap002-rn-crud-lembretes/App.tsx
+++ ap002-rn-crud-lembretes/App.tsx
@@ -1,5 +1,6 @@
+import { FontAwesome } from '@expo/vector-icons';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
+import { Alert, StyleSheet, Text, View, TextInput, Button, Pressable, FlatList, Linking } from 'react-native';
 
 interface Lembrete{
   id: string;
@@ -9,6 +10,7 @@ interface Lembrete{
 export default function App() {
 
   // vetor que contém, um estado com uma função
+  //useState aqui é um hook
   const [lembrete, setLembrete] = useState<string>('')
   const [lembretes, setLembretes] = useState<Lembrete[]>([])
 
@@ -19,10 +21,41 @@ export default function App() {
     }
 
     setLembretes(lembreteAtual => [novoLembrete, ...lembreteAtual])
-
     setLembrete('')
-
   }
+  
+  const abrirGit = () => {
+    Linking.openURL('https://www.github.com/')
+  }
+
+  const remover = (lembrete: Lembrete) => {
+    // Alert.alert(
+      //titulos
+      'Remover lmebrete',
+      `Deseja remover esse lembrete? ${lembrete.texto}`,
+      //lista de botões 
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+
+        {
+          text: 'Remover',
+          style: 'destructive',
+          onPress: () => {
+            setLembretes((lembretesAtual) =>{
+              return lembretesAtual.filter((l) => l.id !== lembrete.id)
+            })
+          }
+        }
+      ]
+    // )
+
+    setLembretes(lembretesAtual => lembretesAtual.filter(l => l.id !== lembrete.id))
+  }
+
+  
 
   return (
     <View style={styles.container}>
@@ -36,9 +69,35 @@ export default function App() {
           style={styles.button}>
           <Text style={styles.buttonText} >Salvar</Text>  
         </Pressable> 
-        {
-          lembretes.map(l => <Text>{l.texto}</Text>)
-        }
+        <FlatList
+          style={styles.list}
+          keyExtractor={(l) => l.id}
+          data={lembretes}
+          ItemSeparatorComponent={() => <View style={{marginVertical: 4}}></View>}
+          // ListFooterComponent={() => <FontAwesome name='github' size={40}/>}
+          renderItem={(l) => {
+            return (
+              <View style={styles.listItem}>
+                <Text
+                  style={styles.listItemText}>
+                  {l.item.texto}
+                </Text>
+                <Pressable
+                  style={styles.listItemButtons}>
+                  <FontAwesome name='pencil' size={24}/>
+                </Pressable>
+                <Pressable
+                  onPress={() => remover(l.item)}>
+                  <FontAwesome name='trash-o' size={24}/>
+                </Pressable>
+              </View>
+            )
+          }}/>
+          <Pressable
+            onPress={abrirGit}
+            style={{borderWidth: 1, borderColor: '#eee', borderRadius: 4, width: '80%', alignItems: 'center', marginTop: 12}}>
+            <FontAwesome name='github' size={40}/>
+          </Pressable>
     </View>
   );
 }
@@ -48,7 +107,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    paddingVertical: 18
   },
 
   input: {
@@ -71,6 +131,48 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     textAlign: 'center',
+  },
+
+  list: {
+    marginTop: 12, 
+    borderWidth: 1, 
+    borderColor: '#DDD', 
+    borderRadius: 8, 
+    width:'80%',
+    padding: 8
+  },
+
+  listItem:{
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBlockColor: '#EEE',
+    backgroundColor: '#F0F0F0',
+    textAlign: 'center',
+    borderRadius: 4,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  listItemText:{
+    textAlign: 'center',
+    width: '70%'
+  },
+
+  listItemButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '30%'
   }
 
+
 });
+
+
+// [
+//   {
+//     item: {
+//       id: 1,
+//       texto: 'abc'
+//     }
+//   },  
+// ]
